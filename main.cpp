7585367fa090ad85b0f2bc31f9fdfdd403ac45d1
ian <iflaskamp@carthage.edu>
2025-03-23 18:17:13 -0500

    Added support for mantissa and fixed bugs in checkValidInput()

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 //required function prototypes
 bool characteristic(const char numString[], int& c);
 bool mantissa(const char numString[], int& numerator, int& denominator);
-bool checkValidInput(const char currentChar, const int pos);
+bool checkValidInput(const char currentChar, const char nextChar, const int pos);
 
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len); 
@@ -17,7 +17,7 @@ int main()
 {
     //this c-string, or array of 8 characters, ends with the null terminating character '\0'
     //['1', '2', '3', '.', '4', '5', '6', '\0']
-    const char number[] = "123.456"; 
+    const char number[] = "1.2.3"; 
     int c, n, d;
 
     //if both conversions from c-string to integers can take place
@@ -88,8 +88,8 @@ bool characteristic(const char numString[], int& c)
     //Keep checking characters until we reach a decimal point or end of c-string if the number is just a integer 
     while(numString[i] != '.' && numString[i] != '\0') 
     {
-        //Make sure the input for the current character is valid
-        if(checkValidInput(numString[i], i))
+        //Make sure the input for the current character is valid, use the current character, next character, and position.
+        if(checkValidInput(numString[i], numString[i+1], i))
         {
             //If there is a negative sign (pos 0)
             if(numString[i] == '-' && i == 0) 
@@ -101,7 +101,7 @@ bool characteristic(const char numString[], int& c)
             else if(numString[i] >= '0' && numString[i] <= '9') 
             {
                 //Multiply the previous number by 10 to add the next digit in the number
-                c = c * 10;
+                c *= 10;
                 //Subtract the current character from '0' to get their integer type.
                 c += numString[i] - '0';
             }
@@ -118,32 +118,9 @@ bool characteristic(const char numString[], int& c)
     }
 
     //If there was a negative sign and the input was valid, change the sign of c
-    if(isNegative && ret) {
+    if(isNegative && ret) 
+    {
         c = -c;
-    }
-
-    return ret;
-}
-//--
-bool checkValidInput(const char currentChar, const int pos)
-{
-    //Return value
-    bool ret = false;
-    
-    //If a char is a '0'-'9'
-    if(currentChar >= '0' && currentChar <= '9')
-    {
-        ret = true;
-    }
-    //If the first char is a sign +/-
-    else if((currentChar == '+' || currentChar == '-') && pos == 0)
-    {
-        ret = true;
-    }
-    //If a char is a space or decimal
-    else if(currentChar == ' ' || currentChar == '.') 
-    {
-        ret = true;
     }
 
     return ret;
@@ -151,11 +128,87 @@ bool checkValidInput(const char currentChar, const int pos)
 //--
 bool mantissa(const char numString[], int& numerator, int& denominator)
 {
-    //hard coded return value to make the main() work
-    numerator = 456;
-    denominator = 1000;
-    return true;
+    int i = 0;
+
+    //Set numerator to 0 and denominator to 1 assuming there is no mantissa (base case)
+    numerator = 0;
+    denominator = 1;
+
+    //Bool to start parsing the mantissa once we reach a '.'
+    bool startParsing = false;
+    //Return bool for valid input
+    bool ret = true;
+
+    while(numString[i] != '\0') {
+        //Make sure the input for the current character is valid
+        if(checkValidInput(numString[i], numString[i+1], i))
+        {
+            //If we've already reached a '.'
+            if(startParsing)
+            {
+                //For every number after the decimal
+                if(numString[i] >= '0' && numString[i] <= '9') 
+                {
+                    //Same logic as the characteristic() function 
+                    numerator *= 10;
+                    numerator += numString[i] - '0';
+
+                    //Increase denominator by 10
+                    denominator*= 10;
+                    
+                }
+            }
+
+            //Else check if the current character is a '.'
+            else if(numString[i] == '.' && !startParsing) 
+            {
+                //Ready to parse
+                startParsing = true;
+            }
+
+            i++;
+        }
+
+        //Otherwise return an error on input message
+        else 
+        {
+            ret = false;
+            break;
+        }
+    }
+
+    //If we have a bunch of trailing zeros after the decimal for some reason, make sure the denominator is 1
+    if(numerator == 0) 
+    {
+        denominator = 1;
+    }
+
+    return ret;
 }
+//--
+bool checkValidInput(const char currentChar, const char nextChar, const int pos)
+{
+    //Return value assuming the input is invalid
+    bool ret = false;
+
+    //If a char is a '0'-'9'
+    if(currentChar >= '0' && currentChar <= '9')
+    {
+        ret = true;
+    }
+    //If the first char is a sign +/-, the next character must be a number or decimal
+    else if((currentChar == '+' || currentChar == '-') && ((nextChar >= '0' && nextChar <= '9') || nextChar == '.') && pos == 0)
+    {
+        ret = true;
+    }
+    //If a char is a space or decimal. If a decimal there must be a number after it.
+    else if(currentChar == ' ' || (currentChar == '.' && (nextChar >= '0' && nextChar <= '9')))
+    {
+        ret = true;
+    }
+
+    return ret;
+}    
 //--
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
